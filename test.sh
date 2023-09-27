671566570c0d0a6f71f15c273210faa54ca6085f
abhay4291 <63400734+abhay4291@users.noreply.github.com>
2023-09-27 15:57:25 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,31 @@
 pipeline {
-  agent any
+    agent any
 
-  stages {
-    stage('run command') {
-      steps {
-        script {
-          // Run the command to create the file on the remote server using 'sh' step
-          sh """
-            ssh -i /var/lib/jenkins/.ssh/uat-key -o StrictHostKeyChecking=no ec2-user@172.31.3.232 'touch /home/ec2-user/anuj'
-          """
+    stages {
+        stage('Execute Script on Remote Server') {
+            steps {
+                script {
+                    // SSH into the remote server
+                    sshagent(credentials: ['jenkins']) {
+                        // Connect to the remote server
+                        def remoteServer = 'ec2-user@172.31.3.232'
+
+                        // Command to download the script from GitHub
+                        def downloadCommand = """
+                            ssh ${remoteServer} 'wget -O /tmp/script.sh https://github.com/abhay4291/abhay-test/raw/main/script.sh'
+                        """
+
+                        // Execute the download command
+                        sh downloadCommand
+
+                        // Make the downloaded script executable
+                        sh "ssh ${remoteServer} 'chmod +x /tmp/script.sh'"
+
+                        // Execute the script on the remote server
+                        sh "ssh ${remoteServer} '/tmp/script.sh'"
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
