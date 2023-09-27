b997f2d1ddd08d62cea56abb68281992b1b444e3
abhay4291 <63400734+abhay4291@users.noreply.github.com>
2023-09-27 15:32:11 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,16 @@
-#!/bin/bash
+pipeline {
+  agent any
+
+  stages {
+    stage('run command') {
+      steps {
+        script {
+          // Run the command to create the file on the remote server using 'sh' step
+          sh """
+            ssh -i /var/lib/jenkins/.ssh/uat-key -o StrictHostKeyChecking=no ec2-user@172.31.3.232 'touch /home/ec2-user/anuj'
+          """
+        }
+      }
+    }
+  }
+}
